470bff549aecba7f97cd7e2a24fdfe2213e4ffec
TrietHuynh0404 <145630703+TrietHuynh0404@users.noreply.github.com>
2023-12-13 08:46:47 +0700

    add infor activity

---
+++ app/src/main/java/com/example/phuotogether/gui_layer/info/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.phuotogether.gui_layer.info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.phuotogether.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
